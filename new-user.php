abc11897948a46f4e9f0039b100870f7f4a1afd8
white-collor <krishsiva105@gmail.com>
2021-11-20 11:45:40 +0530

    second commit code

---
+++ new-user.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="x-apple-disable-message-reformatting">
+	<title></title>
+	<!--[if mso]>
+	<noscript>
+		<xml>
+            <o:OfficeDocumentSettings>
+				<o:PixelsPerInch>96</o:PixelsPerInch>
+			</o:OfficeDocumentSettings>
+		</xml>
+	</noscript>
+	<![endif]-->
+	<style>
+		table, td, div, h1, p {font-family: Arial, sans-serif;}
+	</style>
+</head>
+<body style="margin:0;padding:0;">
+<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+		<tr>
+			<td align="center" style="padding:0;">
+				<table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
+					<tr>
+						<td align="center" style="padding:30px 0 0px 0;background:#ffffff;">
+							<img src="assets/img/logo.png" alt="" width="300" style="height:auto;display:block;" />
+						</td>
+					</tr>
+					<tr>
+						<td style="padding:20px 30px 20px 30px;">
+							<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+								<tr>
+									<td style="padding:0;">
+										<h1 style="text-align: center;">Thanks for Register</h1>
+											<p>your Account is successfully created on the white collar Association website.</p>
+									</td>
+								</tr>
+								<tr>
+									<td style="padding:0;">
+										
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+					<tr>
+						<td style="padding:30px;background:#93282b;">
+							 <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+								<tr>
+									<td style="color:white;" align="left">
+										<p>
+										Dist, Muhavoor,  <br>
+										Tamil Nadu 626111<br><br>
+										<strong>Phone:</strong> 9976649776<br>
+										<strong>Email:</strong> info@example.com<br>
+										</p>
+									</td>
+									<td style="padding:0;width:50%;" align="right">
+										<table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
+											<tr>
+												<td style="padding:0 0 0 10px;width:38px;">
+													<a href="http://www.twitter.com/"><img src="images/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
+												</td>
+												<td style="padding:0 0 0 10px;width:38px;">
+													<a href="http://www.facebook.com/"><img src="images/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
+												</td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
